37e7e8a5110cfdc12cbe32a814b1b2fff415f263
Sandro <Siegfried1997@gmail.com>
2020-04-26 12:55:31 +0300

    ReversingAProcessKata

---
+++ Katas/Katas/6kyu/ReversingAProcess/reversingaprocess.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katas.Katas._6kyu.ReversingAProcess
+{
+    public static class reversingaprocess
+    {
+        public static string Encode(string word,int encoding)
+        {
+
+            return " ";
+        }
+        public static Dictionary<char, int> DictionaryAlphaBetIndexGenerator()
+        {
+            Dictionary<char,int> dictionary = new Dictionary<char, int>();
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                dictionary.Add(alphabet[i],i);
+            }
+            return dictionary;
+        }
+    }
+    
+}
