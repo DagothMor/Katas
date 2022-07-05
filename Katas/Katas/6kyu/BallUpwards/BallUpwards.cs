e590511973e40d1d9365892e7a8ed47bedd2daea
Alexander <siegfried1997@gmail.com>
2022-07-05 21:13:52 +0400

    refactoring

--- Katas/Katas/6kyu/BallUpwards/BallUpwards.cs
+++ Katas/Katas/6kyu/BallUpwards/BallUpwards.cs
@@ -6,29 +6,32 @@ namespace Katas.Katas._6kyu.BallUpwards
 {
 	public static class BallUpwards
 	{
-		public static int Start(int v0)
+        private const double GRAVITATIONAL_EARTH_ACCELERATION = 9.81;
+        private const double KILOMETERS_PER_HOUR = 3.6;
+
+        public static int Start(int v0)
 		{
-			var speed = (decimal)(v0 /3.6);
-			decimal time = 0;
-			var arr = new decimal[] { 0, 0 };
+			var speed = (double)(v0 / KILOMETERS_PER_HOUR);
+			double time = 0;
+			var arr = new double[] { 0, 0 };
 			while (arr[1] >= arr[0])
 			{
 				arr[0] = arr[1];
 				arr[1] = GetHeightByTime(time, speed);
-				time += 0.1;
+                time += 0.1;
 				speed = GetSpeedByTime(time, speed);
 				Console.WriteLine($"{arr[0]}|"+$"|{arr[1]}");
 				Console.WriteLine("___");
 			}
 			return (int)Math.Floor(time);
 		}
-		public static decimal GetHeightByTime(decimal time, decimal speed)
+		public static double GetHeightByTime(double time, double speed)
 		{
-			return speed * time - (decimal) (0.5 * 9.81 * (double) time * (double) time);
+			return speed * time - (double) (0.5 * GRAVITATIONAL_EARTH_ACCELERATION * (double) time * (double) time);
 		}
-		public static decimal GetSpeedByTime(decimal time, decimal speed)
+		public static double GetSpeedByTime(double time, double speed)
 		{
-			return speed - 9.81 * time;
+			return speed - GRAVITATIONAL_EARTH_ACCELERATION * time;
 		}
 	}
 }
